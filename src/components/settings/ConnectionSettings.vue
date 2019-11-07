1d6ceb240dee863ad8ae409f43470e4b0657bfac
KD0NKS <andrew.m.fairhurst@gmail.com>
2019-11-06 18:11:33 -0600

    Added stub for connection settings.

---
+++ src/components/settings/ConnectionSettings.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-expansion-panel>
+        <v-expansion-panel-header>Connection Settings</v-expansion-panel-header>
+        <v-expansion-panel-content>
+
+        </v-expansion-panel-content>
+    </v-expansion-panel>
+</template>
+
+<script type="ts">
+    import store from '../../store';
+
+    export default {
+        data: () => ({
+            
+        })
+        , created() {
+            // load settings here
+            //https://jsfiddle.net/awolf2904/3rabkzsn/1/
+            //store.dispatch('getStationSettings');
+        }
+        , computed: {
+        }
+        , methods: {
+        }
+    }
+</script>
