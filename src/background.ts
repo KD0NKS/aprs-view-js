394fd720d6e923dd50bf0f0f7ee711741803aa20
KD0NKS <andrew.m.fairhurst@gmail.com>
2021-03-17 20:24:29 -0500

    Update dependencies.

--- src/background.ts
+++ src/background.ts
@@ -24,6 +24,7 @@ function createWindow() {
             // Use pluginOptions.nodeIntegration, leave this alone
             // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
             nodeIntegration: (process.env.ELECTRON_NODE_INTEGRATION as unknown) as boolean
+            , contextIsolation: false   // Presumably this has to be false because of electron store
         }
     })
 
